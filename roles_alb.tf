56be627df142d7d47c3710852004644e4a4a6eea
SergioCMDev <sergiocmdev@gmail.com>
2026-02-10 18:06:49 +0100

    chore: añadida politica json para AWS ALB, cambiada logica para usarla en vez de bajarla desde GitHub, Movido logica de OIDC a archivo roles_alb

--- roles_alb.tf
+++ roles_alb.tf
@@ -1,56 +1,37 @@
 #=============================================
-# AWS LOAD BALANCER CONTROLLER ROLE AND POLICY
+# OIDC (OpenId Connect)
 #=============================================
 
-data "http" "aws_load_balancer_controller_policy" {
-  url = "https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/${local.alb_controller_version}/docs/install/iam_policy.json"
-  request_headers = {
-    Accept = "application/json"
-  }
+data "tls_certificate" "eks" {
+  url = aws_eks_cluster.main.identity[0].oidc[0].issuer
 }
 
-resource "aws_iam_policy" "aws_load_balancer_controller_iam_policy" {
-  name        = "AWSLoadBalancerControllerIAMPolicy"
-  description = "IAM policy for AWS Load Balancer Controller"
-  policy      = data.http.aws_load_balancer_controller_policy.response_body
+resource "aws_iam_openid_connect_provider" "eks" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
+  url             = aws_eks_cluster.main.identity[0].oidc[0].issuer
+
   tags = {
-    Source  = "https://github.com/kubernetes-sigs/aws-load-balancer-controller"
-    Version = "${local.alb_controller_version}"
+    Name = "${local.cluster_name}-eks-oidc"
   }
 }
 
-resource "aws_iam_role_policy_attachment" "alb_controller" {
-  role       = aws_iam_role.aws_load_balancer_controller.name
-  policy_arn = aws_iam_policy.aws_load_balancer_controller_iam_policy.arn
-}
-
-
-resource "aws_iam_role_policy" "aws_load_balancer_controller_policy" {
-  name = "ec2_workers_policy"
-  role = aws_iam_role.aws_load_balancer_controller.id
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      "Effect" : "Allow",
-      "Action" : [
-        "elasticloadbalancing:DescribeListenerAttributes"
-      ],
-      "Resource" : "*"
-    }]
-  })
-}
+#=============================================
+# AWS LOAD BALANCER CONTROLLER ROLE AND POLICY
+#=============================================
 
 resource "aws_iam_role" "aws_load_balancer_controller" {
   name = "aws-load-balancer-controller"
 
+  # Trust policy: confía en la ServiceAccount específica
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Action = "sts:AssumeRoleWithWebIdentity"
       Effect = "Allow"
       Principal = {
         Federated = aws_iam_openid_connect_provider.eks.arn
       }
+      Action = "sts:AssumeRoleWithWebIdentity"
       Condition = {
         StringEquals = {
           "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub" = "system:serviceaccount:kube-system:aws-load-balancer-controller"
@@ -59,4 +40,15 @@ resource "aws_iam_role" "aws_load_balancer_controller" {
       }
     }]
   })
+}
+
+resource "aws_iam_policy" "aws_load_balancer_controller_iam_policy" {
+  name        = "AWSLoadBalancerControllerIAMPolicy"
+  description = "IAM policy for AWS Load Balancer Controller"
+  policy      = file("${path.path.module}/iam_policies/alb_controller_policy.json")
+}
+
+resource "aws_iam_role_policy_attachment" "alb_controller" {
+  role       = aws_iam_role.aws_load_balancer_controller.name
+  policy_arn = aws_iam_policy.aws_load_balancer_controller_iam_policy.arn
 }
