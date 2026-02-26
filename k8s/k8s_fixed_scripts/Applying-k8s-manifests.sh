a59f6d1fcc056bb2f699c7aa40ae986d54710064
SergioCMDev <sergiocmdev@gmail.com>
2026-02-26 19:42:16 +0100

    Cambios de nombres en archivos

--- k8s/k8s_fixed_scripts/Applying-k8s-manifests.sh
+++ k8s/k8s_fixed_scripts/Applying-k8s-manifests.sh
@@ -2,12 +2,6 @@
 echo "Applying Kubernetes manifests..."
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ROOT_DIR="$(cd $SCRIPT_DIR/.. && pwd)"
-K8S_MANIFEST_DIR="$ROOT_DIR/rendered/k8s"
-K8S_FIXED_MANIFEST_DIR="$ROOT_DIR/k8s_fixed_manifests"
+K8S_MANIFESTS_DIR="$ROOT_DIR/k8s/manifests"
 
-kubectl apply -f $K8S_MANIFEST_DIR/deployment_blue.yaml
-kubectl apply -f $K8S_MANIFEST_DIR/deployment_green.yaml
-kubectl apply -f $K8S_FIXED_MANIFEST_DIR/service_blue.yaml
-kubectl apply -f $K8S_FIXED_MANIFEST_DIR/service_green.yaml
-
-kubectl apply -f $K8S_MANIFEST_DIR/ingress.yaml
+kubectl apply -k $K8S_MANIFESTS_DIR/overlays/eks/kustomize.yaml
