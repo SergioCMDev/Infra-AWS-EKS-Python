b8b695f37b2073f7c8e2247d8e1ac10ff418026d
SergioCMDev <sergiocmdev@gmail.com>
2026-02-26 19:44:06 +0100

    Arreglo de ruta de kustomize

--- k8s/k8s_fixed_scripts/Applying-k8s-manifests.sh
+++ k8s/k8s_fixed_scripts/Applying-k8s-manifests.sh
@@ -4,4 +4,4 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ROOT_DIR="$(cd $SCRIPT_DIR/.. && pwd)"
 K8S_MANIFESTS_DIR="$ROOT_DIR/k8s/manifests"
 
-kubectl apply -k $K8S_MANIFESTS_DIR/overlays/eks/kustomize.yaml
+kubectl apply -k $K8S_MANIFESTS_DIR/overlays/eks/
