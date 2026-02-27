dbdde52c4f2589d94b4fe68143e78a62ffa6f34f
SergioCMDev <sergiocmdev@gmail.com>
2026-02-27 19:56:25 +0100

    Añadidos ingress para minikube

--- k8s/k8s_fixed_scripts/Applying-k8s-manifests-minikube.sh
+++ k8s/k8s_fixed_scripts/Applying-k8s-manifests-minikube.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-echo "Applying Kubernetes manifests..."
+echo "Applying Kubernetes manifests for minikube..."
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ROOT_DIR="$(cd $SCRIPT_DIR/.. && pwd)"
 K8S_MANIFESTS_DIR="$ROOT_DIR/manifests"
 # echo "DIR $K8S_MANIFESTS_DIR"
 
 kubectl apply -k $K8S_MANIFESTS_DIR/overlays/minikube/
+kubectl apply -f $K8S_MANIFESTS_DIR/base/ingress-green.yaml
+kubectl apply -f $K8S_MANIFESTS_DIR/base/ingress-blue.yaml
